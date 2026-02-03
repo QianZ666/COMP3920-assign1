ecdb74937d838db2e3a0e84edddf0d49ebc70942
Qian Zhang <qzhang116@my.bcit.ca>
2026-02-03 15:21:27 -0800

    update brcypt

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const mysql = require("mysql2/promise");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const saltRounds = 12;
 
 const port = process.env.PORT || 3000;
